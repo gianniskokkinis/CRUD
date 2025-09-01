56e4ffa2a1ce7fa234128fe926ce8c53fee0e5df
GiannisKokkinis <45635227+gianniskokkinis@users.noreply.github.com>
2025-09-01 21:16:40 +0300

    add the entities and relations annotation

--- crud/app/models/Product.java
+++ crud/app/models/Product.java
@@ -5,20 +5,27 @@ import java.sql.Date;
 import jakarta.persistence.*;
 
 
+@Entity
+@Table(name="users")
 public class Product {
 	
 	@Id
-	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@Column(name="username")
 	private String name;
 	
+	@Column(name="description")
 	private String description; 
 	
+	@Column(name="price")
 	private double price;
 	
+	@Column(name="date_add")
 	private Date addDate;
 	
+	@Column(name="date_upd")
 	private Date updateDate;
 
 }
